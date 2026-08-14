5b88b89e482a3e86666e8703ae86ce43c7d3e689
dvijeniii05 <adikbsw@gmail.com>
2026-08-14 15:14:36 +0100

    ⚗️ Experimenting with setting block tol to 400

--- constants/combatTiming.ts
+++ constants/combatTiming.ts
@@ -1,3 +1,3 @@
 // To be experimented, some ideas are here --> architecture/fight-concept.md
 // Valid only before impact: 0 <= attackTime - blockTime <= tolerance.
-export const BLOCK_TOLERANCE_MS = 300;
+export const BLOCK_TOLERANCE_MS = 400;
